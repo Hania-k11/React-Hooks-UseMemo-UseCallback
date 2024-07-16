af12bac0f476cd498ef7f93a7b6db223d47805e9
Hania <haniaaa0987@example.com>
2024-07-16 10:34:46 +0500

    push

---
+++ useCallback/src/App.jsx
@@ -0,0 +1,46 @@
+
+import './App.css'
+import { useCallback, useState } from 'react';
+import Search from './Search';
+
+const allUsers = [
+  'john',
+  'alex',
+  'george',
+  'simon',
+  'james',
+];
+
+export default function Demo() {
+  const [users, setUsers] = useState(allUsers);
+
+  const handleSearch = useCallback(
+    (text) => {
+      console.log(users[0]);
+
+      const filteredUsers = allUsers.filter((user) =>
+        user.includes(text)
+      );
+      setUsers(filteredUsers);
+    },
+    [users]
+  );
+
+  return (
+    <div className='tutorial'>
+      <div className='align-center mb-2 flex'>
+        <button onClick={() => setUsers(shuffle(allUsers))}>
+          Shuffle
+        </button>
+
+        <Search onChange={handleSearch} />
+      </div>
+      <ul>
+        {users.map((user) => (
+          <li key={user}>{user}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+f
